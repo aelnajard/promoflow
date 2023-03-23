d5f3fb4010c18c920439b3ec0c7b7a3ed2e871dc
Alejandra Rozo <aelnajard@192.168.0.5>
2023-03-23 06:39:24 -0500

    ...

--- Home/index.php
+++ Home/index.php
@@ -267,7 +267,7 @@
           },
           success: function(data){
             var data = jQuery.parseJSON(data);
-            alert(data[0]);
+            alert(data["COUNT(*)"]);
 
          }
         }
