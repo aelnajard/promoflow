19930f0d9bb9c05020e0e6a630c9df314fbba730
Alejandra Rozo <aelnajard@192.168.0.5>
2023-03-23 06:00:19 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -10,10 +10,8 @@ require_once('../Models/Users.php');
       $user->setEmail($_POST['email']);
       $user->setPassword($_POST['password']);
       $user->readUserExist();
-      echo $_POST['email']."string".$_POST['password'];
-
-      //echo json_encode($user->readUserExist());
-      //  $_SESSION["withSelected"] = $_POST['value'];
+    //  echo $_POST['email']."string".$_POST['password'];
+      echo json_encode($user->readUserExist());
     }
 
  ?>
