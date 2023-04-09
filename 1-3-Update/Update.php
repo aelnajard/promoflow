1f89482f26d3ce24fe82f5f5e329dcf88c1cbc08
Andres David <rozoad@globalhitss.com>
2023-04-09 08:51:48 -0500

    ...

--- 1-3-Update/Update.php
+++ 1-3-Update/Update.php
@@ -4,6 +4,13 @@
     width: 80%;
     background-color: brown;
   }
+  .containerCRUDTitle h2{
+    color: white;
+    font-size: 1.1em;
+    font-weight: 300;
+    text-align: center;
+    background-color: green;
+  }
   .containerCRUDWrap{
     position: relative;
     width: 90%;
@@ -14,7 +21,7 @@
 </style>
 <div class="ContainerCRUD">
   <div class="containerCRUDTitle">
-      <h2>Update user</h2>
+      <h2>Update user.</h2>
   </div>
 <!--<div class="containerCRUDWrap">
 
