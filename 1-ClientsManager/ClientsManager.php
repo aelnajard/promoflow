af0e15f282062301a545b2fecd15861daa081eaf
aelnajard <aelnajard@gmail.com>
2023-04-12 05:43:53 -0700

    ...

--- 1-ClientsManager/ClientsManager.php
+++ 1-ClientsManager/ClientsManager.php
@@ -127,6 +127,11 @@
   width: 100%;
   height: 100px;
   background-color: blue;
+
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction:column;
 }
 .inputFormUpdate{
   position:relative;
