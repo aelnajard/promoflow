1cf67fe62f05a4cda54f437caa98a7304af0ac96
Andres David <rozoad@globalhitss.com>
2023-04-13 16:06:55 -0500

    ...

--- 0-Messages/Messages.php
+++ 0-Messages/Messages.php
@@ -8,12 +8,34 @@
     background-color: rgba(0, 0, 0, 0.6);
     z-index: 100;
   }
+  .relative{
+    position: relative;
+  }
+  .message{
+    position: relative;
+    width: 300px;
+    height: 200px;
+    background-color: rgba(225, 228, 235, 0.9);
+  }
+  .headMessage{
+    position: relative;
+    height: 20%;
+    width: 100%;
+    background-color: rgba(47, 69, 96, 0.9);
+  }
 </style>
 
 
-<section class="Message">
+<section >
   <div class="messageBackground">
-
+    <div class="relative">
+      <div class="message">
+        <div class="headMessage">
+          <h2>Promoflow</h2>
+          <img src="../0-Messages/Images/Logo.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 
