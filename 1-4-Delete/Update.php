801fe78acdd8c92f65c14bc9d3f6cb47715547a0
aelnajard <aelnajard@gmail.com>
2023-04-16 13:43:10 -0700

    ...

--- 1-4-Delete/Update.php
+++ 1-4-Delete/Update.php
@@ -109,10 +109,10 @@ function setUsersDelete(data){
   }
 var globalIdUser;
   function deleteUsers(idUser) {
-    valueButtonSelected = -1;
     message(2, "Delete", "Cancel", "Would you like to confirm the deletion of this user?");
-    alert(valueButtonSelected);
+
     if (valueButtonSelected == 0) {
+
       globalIdUser  = idUser;
       $.ajax( "../App/Controller/Controller2.php", {
              type: 'post',
