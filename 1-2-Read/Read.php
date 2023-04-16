2d361fe1a3ab774a4ad34c1ca9404d02c8a4c8b1
aelnajard <aelnajard@gmail.com>
2023-04-16 11:22:33 -0700

    ...

--- 1-2-Read/Read.php
+++ 1-2-Read/Read.php
@@ -153,7 +153,7 @@
          '<div class="boxUser" >'+
              '<div class="boxUserName">'+
                '<h3 class="nameUserRead">'+data[i]["name"]+'</h3>'+
-               '<div class="toggleInformation" onclick="toggleInformation3(\''+data[i]["idUser"]+'\')>'+
+               '<div class="toggleInformation" >'+
                  '<img class="buttonPlus"src="../1-2-Read/Images/mas.png" alt="">'+
                  '<img class="buttonLess"src="../1-2-Read/Images/menos.png" alt="">'+
                '</div>'+
@@ -197,10 +197,6 @@
         buttonLess[i].classList.toggle("showHide");
       })
 
-    }
-
-    function toggleInformation3(data2){
-      alert(data2);
     }
 
 
