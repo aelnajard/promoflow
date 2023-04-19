5781dc680a2f0416c400691edeec0c9922d044aa
Andres David <rozoad@globalhitss.com>
2023-04-19 18:23:40 -0500

    ...

--- App/Models/Users.php
+++ App/Models/Users.php
@@ -30,7 +30,7 @@
       function readUserExist(){
        try{
         $sql = $this->conn->conn()->query("SELECT COUNT(*) FROM `Users` WHERE `email` = '$this->email' AND  `password` = '$this->password'");
-        $data = $sql->fetch(PDO::FETCH_OBJ);
+        $data = $sql->fetch(PDO::FETCH_ASSOC);
         $this->conn->close();
         return $data;
             }
