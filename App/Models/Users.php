939b09d74ab615f52d03e767a7015c330fa7c4d3
Andres David <rozoad@globalhitss.com>
2023-04-20 15:38:12 -0500

    ...

--- App/Models/Users.php
+++ App/Models/Users.php
@@ -89,7 +89,7 @@
         function verifyRepeatUser(){
           try{
            $sql = $this->conn->conn()->query("SELECT COUNT(*) FROM `Users` WHERE `email` = '$this->email'");
-           $data = $sql->fetch(PDO::FETCH_BOTH);
+           $data = $sql->fetch(PDO::FETCH_OBJ);
            $this->conn->close();
            return $data->count;
                }
