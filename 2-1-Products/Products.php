5b4ae12fdecc7ca442443a47a9d233d123902db7
aelnajard <aelnajard@gmail.com>
2023-04-24 19:53:39 -0700

    ...

--- 2-1-Products/Products.php
+++ 2-1-Products/Products.php
@@ -142,7 +142,7 @@
     position: relative;
     width: 100%;
     height: 40px;
-    background-color: rgba(225, 228, 235, 0.5);
+    background-color: rgba(225, 228, 235, 0.6);
     border: 1px solid black;
   }
   .ProductoInformationSubitemNameW3P h3{
