2cdf465dfd346d511deefc5ba9d5654f73081834
Andres David <rozoad@globalhitss.com>
2023-04-25 23:04:52 -0500

    ...

--- 2-1-Products/Products.php
+++ 2-1-Products/Products.php
@@ -304,7 +304,7 @@
              module: "requestProductsExample"
                    },
            success: function(data){
-             alert(data);
+             console.log(data);
           }
          }
         )
