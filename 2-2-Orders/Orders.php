dc30fee352324605f7275f43c24efcef4c625e7e
aelnajard <aelnajard@gmail.com>
2023-04-29 08:24:03 -0700

    ...

---
+++ 2-2-Orders/Orders.php
@@ -0,0 +1,285 @@
+<style media="screen">
+  .boxBigCategoryW3P{
+    position: relative;
+    width: 100%;
+  }
+  .boxBigCategoryNameW3P{
+    position: relative;
+    width: 100%;
+    height: 40px;
+    background-color: rgba(47, 69, 96, 1);
+    border: 1px solid black;
+  }
+  .boxBigCategoryNameW3P h3{
+    position: relative;
+    text-align: center;
+    color: white;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .openToggleBigCategoryW3P{
+    position: absolute;
+    width: 40px;
+    height: 100%;
+    top: 0px;
+    right: 0px;
+  }
+  .boxCategoryW3P{
+    position: relative;
+    width: 100%;
+  }
+  .boxCategoryNameW3P{
+    position: relative;
+    width: 100%;
+    height: 40px;
+    background-color: rgba(47, 69, 96, 0.7);
+    border: 1px solid black;
+  }
+  .boxCategoryNameW3P h3{
+    position: relative;
+    text-align: center;
+    color: white;
+    top: 50%;
+    transform: translateY(-50%);
+    font-weight: 500;
+  }
+  .openToggleCategoryW3P{
+    position: absolute;
+    width: 40px;
+    height: 100%;
+    top: 0px;
+    right: 0px;
+  }
+  .boxGroupProductW3P{
+    position: relative;
+    width: 100%;
+  }
+  .boxGroupProductNameW3P{
+    position: relative;
+    width: 100%;
+    height: 40px;
+    background-color: rgba(47, 69, 96, 0.4);
+    border: 1px solid black;
+  }
+  .boxGroupProductNameW3P h3{
+    position: relative;
+    text-align: center;
+    color: white;
+    top: 50%;
+    transform: translateY(-50%);
+    font-weight: 300;
+  }
+  .openToggleGroupProductW3P{
+    position: absolute;
+    width: 40px;
+    height: 100%;
+    top: 0px;
+    right: 0px;
+  }
+  .boxProductW3P{
+    position: relative;
+    width:100%;
+  }
+  .boxProductNameW3P{
+    position: relative;
+    width: 100%;
+    height: 40px;
+    background-color: rgba(47, 69, 96, 0.1);
+    border: 1px solid black;
+  }
+  .boxProductNameW3P h3{
+    position: relative;
+    text-align: center;
+    color: white;
+    top: 50%;
+    transform: translateY(-50%);
+    font-weight: 100;
+  }
+  .openToggleProductW3P{
+    position: absolute;
+    width: 40px;
+    height: 100%;
+    top: 0px;
+    right: 0px;
+  }
+
+
+
+  .boxProductInformationW3P{
+    position: relative;
+    width: 100%;
+  }
+  .itemProductInformationNameW3P{
+    position: relative;
+    width: 100%;
+    height: 40px;
+    background-color: rgba(225, 228, 235, 0.9);
+    border: 1px solid black;
+  }
+  .itemProductInformationNameW3P h3{
+    position: relative;
+    text-align: center;
+    color: Black;
+    top: 50%;
+    transform: translateY(-50%);
+    font-weight: 300;
+  }
+  .openToggleProductInformationW3P{
+    position: absolute;
+    width: 40px;
+    height: 100%;
+    top: 0px;
+    right: 0px;
+  }
+
+
+  .ProductoInformationSubitemW3P{
+    position: relative;
+    width: 100%;
+  }
+
+  .ProductoInformationSubitemNameW3P{
+    position: relative;
+    width: 100%;
+    height: 40px;
+    background-color: rgba(225, 228, 235, 0.8);
+    border: 1px solid black;
+  }
+  .ProductoInformationSubitemNameW3P h3{
+    position: relative;
+    text-align: center;
+    color: Black;
+    top: 50%;
+    transform: translateY(-50%);
+    font-weight: 300;
+  }
+
+</style>
+<!--  Table 1  -->
+
+<div class="containerTableW3P">
+  <div class="containerCenterTable">
+
+
+      <div class="boxBigCategoryW3P">
+        <div class="boxBigCategoryNameW3P">
+          <h3>Big Category Name</h3>
+          <div class="openToggleBigCategoryW3P">
+            <img class="buttonPlus"src="../2-1-Products/Images/mas.png" alt="">
+            <img class="buttonLess"src="../2-1-Products/Images/menos.png" alt="">
+          </div>
+        </div>
+        <div class="boxCategoryW3P">
+          <div class="boxCategoryNameW3P">
+            <h3>Category Name</h3>
+            <div class="openToggleCategoryW3P">
+              <img class="buttonPlus"src="../2-1-Products/Images/mas.png" alt="">
+              <img class="buttonLess"src="../2-1-Products/Images/menos.png" alt="">
+            </div>
+          </div>
+          <div class="boxGroupProductW3P">
+            <div class="boxGroupProductNameW3P">
+              <h3>Group products</h3>
+              <div class="openToggleGroupProductW3P">
+                <img class="buttonPlus"src="../2-1-Products/Images/mas.png" alt="">
+                <img class="buttonLess"src="../2-1-Products/Images/menos.png" alt="">
+              </div>
+            </div>
+            <div class="boxProductW3P">
+              <div class="boxProductNameW3P">
+                <h3>Product</h3>
+                <div class="openToggleProductW3P">
+                  <img class="buttonPlus"src="../2-1-Products/Images/mas.png" alt="">
+                  <img class="buttonLess"src="../2-1-Products/Images/menos.png" alt="">
+                </div>
+              </div>
+              <div class="boxProductInformationW3P">
+                <div class="itemProductInformationNameW3P">
+                  <h3>Width</h3>
+                </div>
+                <div class="itemProductInformationNameW3P">
+                  <h3>Height</h3>
+                </div>
+                <div class="itemProductInformationNameW3P">
+                  <h3>Bleed</h3>
+                </div>
+                <div class="itemProductInformationNameW3P">
+                  <h3>Rununits</h3>
+                </div>
+                <div class="itemProductInformationNameW3P">
+                  <h3>Reverse</h3>
+                </div>
+                <div class="itemProductInformationNameW3P">
+                  <h3>Allowmulti</h3>
+                </div>
+                <div class="itemProductInformationNameW3P">
+                  <h3>Partnercode</h3>
+                </div>
+                <div class="itemProductInformationNameW3P">
+                  <h3>Prices</h3>
+                  <div class="openToggleProductInformationW3P">
+                    <img class="buttonPlus"src="../2-1-Products/Images/mas.png" alt="">
+                    <img class="buttonLess"src="../2-1-Products/Images/menos.png" alt="">
+                  </div>
+                </div>
+                <div class="ProductoInformationSubitemW3P">
+                  <div class="ProductoInformationSubitemNameW3P">
+                    <h3>Price 1</h3>
+                  </div>
+                  <div class="ProductoInformationSubitemNameW3P">
+                    <h3>Price 2</h3>
+                  </div>
+                </div>
+
+              </div><!-- end  Product Information -->
+
+            </div><!-- end  Product W3P -->
+
+          </div> <!-- end Group Product -->
+
+        </div><!-- end Category -->
+
+      </div> <!-- end Big Category -->
+
+
+<!--
+  <table>
+    <thead>
+      <tr>
+        <th>Code</th>
+        <th>Name</th>
+
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>BAGS</td>
+        <td>Bags Zone</td>
+
+      </tr>
+      <tr>
+        <td>GIVEAWAY</td>
+        <td>Giveaway Gifts Zone</td>
+
+      </tr>
+      <tr>
+        <td>PACKGING</td>
+        <td>Packaging Zone</td>
+
+      </tr>
+      <tr>
+        <td>SPECLPRJ</td>
+        <td>Special Projects **Offline Only** Zone</td>
+
+      </tr>
+      <tr>
+        <td>XMAS</td>
+        <td>The Colour of Christmas Zone</td>
+
+      </tr>
+
+    </tbody>
+  </table>
+-->
+</div>
+</div>
