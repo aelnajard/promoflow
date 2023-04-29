d3fa14eca0879713a2dfbd03a8a736aa01395159
aelnajard <aelnajard@gmail.com>
2023-04-29 08:33:28 -0700

    ...

--- 2-W3P/W3P.php
+++ 2-W3P/W3P.php
@@ -6,10 +6,10 @@
   <div class="navigateTable">
     <img id="leftArrowW3P" src="Images/arrow.png" alt="">
     <div class="textNavigatorTable">
-      <h3>Big category products</h3>
+      <h3>Products</h3>
     </div>
     <div class="textNavigatorTable">
-      <h3>Category products</h3>
+      <h3>Orders</h3>
     </div>
   <!--  <div class="textNavigatorTable">
       <h3>Group products</h3>
