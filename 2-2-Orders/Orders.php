75d3312ae8049b93115d9a27cee743c21d6b60e6
aelnajard <aelnajard@gmail.com>
2023-04-29 09:37:25 -0700

    ...

--- 2-2-Orders/Orders.php
+++ 2-2-Orders/Orders.php
@@ -66,7 +66,28 @@
         </div>
       </div>
 
-      
+
+    </div>
+
+    <div class="containerItemsOrder">
+
+
+
+      <div class="itemOrder">
+        <h3>Order: 12 - 01 - 2023</h3>
+        <div class="openToggleBigCategoryW3P">
+          <img class="buttonPlus"src="../2-1-Products/Images/mas.png" alt="">
+          <img class="buttonLess"src="../2-1-Products/Images/menos.png" alt="">
+        </div>
+      </div>
+
+      <div class="containerSubitemOrder">
+        <div class="subitemOrder">
+          <h3>id: 234535</h3>
+        </div>
+      </div>
+
+
     </div>
 
 
