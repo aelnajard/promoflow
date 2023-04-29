b1682f1da532e55aa23fbd0bf3c6ef0cd0cab0d9
aelnajard <aelnajard@gmail.com>
2023-04-29 08:49:41 -0700

    ...

--- 2-2-Orders/Orders.php
+++ 2-2-Orders/Orders.php
@@ -10,6 +10,13 @@
     background-color: rgba(47, 69, 96, 1);
     border: 1px solid black;
   }
+  .itemOrder h3{
+    position: relative;
+    text-align: center;
+    color: white;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 
 
 </style>
@@ -20,7 +27,7 @@
 
     <div class="containerItemsOrder">
       <div class="itemOrder">
-
+        <h3>Order: 12 - 01 - 2023</h3>
       </div>
     </div>
 
