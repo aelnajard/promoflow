55bb3a169143664c93512167c3e5bab6dee019a4
aelnajard <aelnajard@gmail.com>
2023-05-04 19:12:05 -0700

    ...

--- 2-2-Orders/Orders.php
+++ 2-2-Orders/Orders.php
@@ -58,7 +58,6 @@
     width: 100%;
     height: 40px;
     background-color: rgba(230, 240, 245, 0.9);
-
     border: 1px solid black;
   }
   .subitemOrder h3{
@@ -82,7 +81,7 @@
     position: relative;
     width: 100%;
     height: 40px;
-    background-color: rgba(225, 228, 235, 0.8);
+    background-color: rgba(230, 240, 245, 0.75);
     border: 1px solid black;
   }
   .subitemOrder2 h3{
